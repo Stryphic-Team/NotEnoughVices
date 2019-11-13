9fd4c62334532f7593573a98862a1c19c4db6c5f
DeaSTL <jhartway99@gmail.com>
2019-11-13 14:06:24 -0500

    Added forge files

---
+++ src/main/java/com/stryphic/notenoughvices/NotEnoughVices.java
@@ -0,0 +1,27 @@
+package com.stryphic.notenoughvices;
+
+import com.stryphic.notenoughvices.utils.Reference;
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class NotEnoughVices {
+    public static Logger logger;
+
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
---
+++ src/main/java/com/stryphic/notenoughvices/utils/Reference.java
@@ -0,0 +1,7 @@
+package com.stryphic.notenoughvices.utils;
+
+public class Reference {
+    public static final String MODID = "notenoughvices";
+    public static final String NAME = "Not Enough Vices";
+    public static final String VERSION = "0.0.1";
+}
